afd8203e2d39ba73919e239b6d82d0f6460b24c3
Mime Čuvalo <mimecuvalo@gmail.com>
2018-11-16 14:30:55 -0800

    ui: add material-ui

--- packages/react-scripts/template/src/Home.js
+++ packages/react-scripts/template/src/Home.js
@@ -1,3 +1,4 @@
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import React from 'react';
 
@@ -8,9 +9,9 @@ function Home() {
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <Button href="https://reactjs.org" target="_blank" rel="noopener noreferrer" variant="contained" color="primary">
         Learn React
-      </a>
+      </Button>
     </div>
   );
 }
