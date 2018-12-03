77e8f99c604587b5419cb1f67b03911dd5be56e7
Mime Čuvalo <mimecuvalo@gmail.com>
2018-12-03 14:55:42 -0800

    semantics: move page body out of header file

--- packages/react-scripts/template/client/app/App.js
+++ packages/react-scripts/template/client/app/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Footer from './Footer';
 import Header from './Header';
+import Home from '../home/Home';
+import { Route, Switch } from 'react-router-dom';
+import NotFound from '../error/404';
 import React, { Component } from 'react';
+import YourFeature from '../your_feature/Your_Feature';
 
 export default class App extends Component {
   render() {
@@ -10,6 +14,13 @@ export default class App extends Component {
       <div className="App">
         <CssBaseline />
         <Header />
+        <main className="App-main">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/your-feature" component={YourFeature} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
         <Footer />
       </div>
     );
--- packages/react-scripts/template/client/app/Header.js
+++ packages/react-scripts/template/client/app/Header.js
@@ -1,28 +1,21 @@
-import Home from '../home/Home';
-import { Link, Route, Switch } from 'react-router-dom';
-import NotFound from '../error/404';
+import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import styles from './Header.module.css';
-import YourFeature from '../your_feature/Your_Feature';
 
 export default class Header extends Component {
   render() {
     return (
       <header className="App-header">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/your-feature" component={YourFeature} />
-          <Route component={NotFound} />
-        </Switch>
-
-        <ul className={styles.menu}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/your-feature">Your Feature</Link>
-          </li>
-        </ul>
+        <nav>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/your-feature">Your Feature</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
