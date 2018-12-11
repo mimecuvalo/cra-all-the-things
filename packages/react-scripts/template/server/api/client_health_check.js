57a7c9e6a7333868738d5159df2d82d0d49a6c59
Mime Čuvalo <mimecuvalo@gmail.com>
2018-12-11 11:29:07 -0800

    fix up lint error

--- packages/react-scripts/template/server/api/client_health_check.js
+++ packages/react-scripts/template/server/api/client_health_check.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 router.post('/', (req, res) => {
-  if (req.body.appVersion == 'somebadid' || req.body.appTime < 0 /* or before some time you specify as bad */) {
+  if (req.body.appVersion === 'somebadid' || req.body.appTime < 0 /* or before some time you specify as bad */) {
     res.send('bad');
     return;
   }
