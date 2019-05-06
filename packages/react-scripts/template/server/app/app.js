925f8f5bc303c00fd2136bc2149a0958d80ea2d9
Mime Čuvalo <mimecuvalo@gmail.com>
2019-05-06 10:41:39 -0700

    mui: fix production issue with classname hydration.
    since we're using apollo in our app and we do two react tree traversals we need to disable material-ui during the first pass for the classnames to match up later on

--- packages/react-scripts/template/server/app/app.js
+++ packages/react-scripts/template/server/app/app.js
@@ -46,7 +46,11 @@ export default async function render({ req, res, next, assetPathsByType, appName
     },
   });
 
-  const completeApp = (
+  const coreApp = <App user={filteredUser} />;
+  // We need to set leave out Material-UI classname generation when traversing the React tree for
+  // react-apollo data. a) it speeds things up, but b) if we didn't do this, on prod, it can cause
+  // classname hydration mismatches.
+  const completeApp = isApolloTraversal => (
     <IntlProvider locale={locale} messages={translations}>
       <HTMLBase
         apolloStateFn={() => apolloClient.extract()}
@@ -64,11 +68,15 @@ export default async function render({ req, res, next, assetPathsByType, appName
       >
         <ApolloProvider client={apolloClient}>
           <StaticRouter location={req.url} context={context}>
-            <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
-              <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
-                <App user={req.session.user} />
-              </MuiThemeProvider>
-            </JssProvider>
+            {!isApolloTraversal ? (
+              <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+                <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
+                  {coreApp}
+                </MuiThemeProvider>
+              </JssProvider>
+            ) : (
+              coreApp
+            )}
           </StaticRouter>
         </ApolloProvider>
       </HTMLBase>
@@ -77,13 +85,13 @@ export default async function render({ req, res, next, assetPathsByType, appName
 
   // This is so we can do `apolloClient.extract()` later on.
   try {
-    await getDataFromTree(completeApp);
+    await getDataFromTree(completeApp(true /* isApolloTraversal */));
   } catch (ex) {
     next(ex);
     return;
   }
 
-  const renderedApp = renderToString(completeApp);
+  const renderedApp = renderToString(completeApp(false /* isApolloTraversal */));
   if (context.url) {
     res.redirect(301, context.url);
     return;
