9ed121c6374fc0510a7e95c0e5a7a008b1a2f644
Mime Čuvalo <mimecuvalo@gmail.com>
2019-05-09 14:31:37 -0700

    components: add snackbar for project

--- packages/react-scripts/template/client/app/App.js
+++ packages/react-scripts/template/client/app/App.js
@@ -1,18 +1,27 @@
 import './App.css';
 import classNames from 'classnames';
 import clientHealthCheck from './client_health_check';
+import CloseIcon from '@material-ui/icons/Close';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { defineMessages, injectIntl } from '../../shared/i18n';
 import ErrorBoundary from '../error/ErrorBoundary';
 import Footer from './Footer';
 import Header from './Header';
 import Home from '../home/Home';
+import IconButton from '@material-ui/core/IconButton';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import NotFound from '../error/404';
 import React, { Component } from 'react';
+import { SnackbarProvider } from 'notistack';
 import UserContext from './User_Context';
 import YourFeature from '../your_feature/YourFeature';
 
+const messages = defineMessages({
+  close: { msg: 'Close' },
+});
+
 // This is the main entry point on the client-side.
+@injectIntl
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -42,25 +51,33 @@ export default class App extends Component {
     // HACK(all-the-things): we can't get rid of FOUC in dev mode because we want hot reloading of CSS updates.
     // This hides the unsightly unstyled app. However, in dev mode, it removes the perceived gain of SSR. :-/
     const devOnlyHiddenOnLoadStyle = this.state.devOnlyHiddenOnLoad ? { opacity: 0 } : null;
+    const closeAriaLabel = this.props.intl.formatMessage(messages.close);
+    const closeButton = (
+      <IconButton key="close" className="App-snackbar-icon" color="inherit" aria-label={closeAriaLabel}>
+        <CloseIcon />
+      </IconButton>
+    );
 
     return (
       <UserContext.Provider value={this.state.userContext}>
-        <ErrorBoundary>
-          <div className={classNames('App', { 'App-logged-in': this.props.user })} style={devOnlyHiddenOnLoadStyle}>
-            <CssBaseline />
-            <Header />
-            <main className="App-main">
-              <ScrollToTop>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/your-feature" component={YourFeature} />
-                  <Route component={NotFound} />
-                </Switch>
-              </ScrollToTop>
-            </main>
-            <Footer />
-          </div>
-        </ErrorBoundary>
+        <SnackbarProvider action={[closeButton]}>
+          <ErrorBoundary>
+            <div className={classNames('App', { 'App-logged-in': this.props.user })} style={devOnlyHiddenOnLoadStyle}>
+              <CssBaseline />
+              <Header />
+              <main className="App-main">
+                <ScrollToTop>
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/your-feature" component={YourFeature} />
+                    <Route component={NotFound} />
+                  </Switch>
+                </ScrollToTop>
+              </main>
+              <Footer />
+            </div>
+          </ErrorBoundary>
+        </SnackbarProvider>
       </UserContext.Provider>
     );
   }
