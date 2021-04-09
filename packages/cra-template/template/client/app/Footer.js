6c2238d5e04b2466eb74abe9451e82671deb3841
Mime Čuvalo <mimecuvalo@gmail.com>
2021-04-08 23:35:53 -0700

    code splitting: split out auth0 and add suspense back

--- packages/cra-template/template/client/app/Footer.js
+++ packages/cra-template/template/client/app/Footer.js
@@ -2,11 +2,6 @@
 import Help from './Help';
 import { createUseStyles } from 'react-jss';
 
-let Debug = () => null;
-if (process.env.NODE_ENV === 'development') {
-  Debug = require('client/internal/Debug').default;
-}
-
 const useStyles = createUseStyles({
   footer: {
     position: 'fixed',
@@ -25,29 +20,25 @@ export default function Footer() {
     // Conditionally compile this code. Should not appear in production.
     if (process.env.NODE_ENV === 'development') {
       // TODO(mime): Suspense and lazy aren't supported by ReactDOMServer yet (breaks SSR).
-      // TODO(mime): Ever since React 16.10.0 the mismatch of SSR is causing a clientside error :-/
-      //    Re-enable this code later when SSR works for Suspense.
-      return <Debug />;
+      const IS_CLIENT = typeof window !== 'undefined';
+      const Fallback = (
+        <span>
+          <F msg="Loading…" />
+        </span>
+      );
+      let SuspenseWithTemporaryWorkaround;
+      if (IS_CLIENT) {
+        const Debug = lazy(() => import('client/internal/Debug'));
+        SuspenseWithTemporaryWorkaround = (
+          <Suspense fallback={Fallback}>
+            <Debug />
+          </Suspense>
+        );
+      } else {
+        SuspenseWithTemporaryWorkaround = Fallback;
+      }
 
-      // const IS_CLIENT = typeof window !== 'undefined';
-      // const Fallback = (
-      //   <span>
-      //     <F msg="Loading…" />
-      //   </span>
-      // );
-      // let SuspenseWithTemporaryWorkaround;
-      // if (IS_CLIENT) {
-      //   const Debug = lazy(() => import('client/internal/Debug'));
-      //   SuspenseWithTemporaryWorkaround = (
-      //     <Suspense fallback={Fallback}>
-      //       <Debug />
-      //     </Suspense>
-      //   );
-      // } else {
-      //   SuspenseWithTemporaryWorkaround = Fallback;
-      // }
-      //
-      // return SuspenseWithTemporaryWorkaround;
+      return SuspenseWithTemporaryWorkaround;
     }
 
     return null;
